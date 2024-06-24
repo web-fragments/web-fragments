817cd55175c1837a7f735fb7864b2cd88a3c63e2
Igor Minar <i@igor.dev>
2024-06-23 23:57:48 -0700

    feat: support streamed reframing

--- e2e/simple-spa/vite.config.ts
+++ e2e/simple-spa/vite.config.ts
@@ -1,12 +1,15 @@
 import {resolve} from "node:path";
 import { defineConfig } from "vite";
-import reframedViteConfig from "../../reframed/vite.config.js";
 
 export default defineConfig({
   appType: "mpa", // so that Vite returns 404 on fetch to an non-existent .html file
   resolve: {
     alias: {
       reframed: resolve(__dirname, "../../reframed/index.ts"),
+
+      // cross-repo development only!
+      // requires writable-dom checked out as a sibling to `reframed`
+      //"writable-dom": resolve(__dirname, "../../../writable-dom/src/index.ts"),
     },
   },
 });
