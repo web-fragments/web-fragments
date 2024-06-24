773f8a420f9398a0d6f6609a24d3e8abe79eb36f
Igor Minar <iminar@cloudflare.com>
2024-06-24 08:04:12 -0700

    fix(simple-spa): use absolute paths in vite's alias config

--- e2e/simple-spa/vite.config.ts
+++ e2e/simple-spa/vite.config.ts
@@ -1,3 +1,4 @@
+import {resolve} from "node:path";
 import { defineConfig } from "vite";
 import reframedViteConfig from "../../reframed/vite.config.js";
 
@@ -5,7 +6,7 @@ export default defineConfig({
   appType: "mpa", // so that Vite returns 404 on fetch to an non-existent .html file
   resolve: {
     alias: {
-      reframed: "../../../reframed/index.ts",
+      reframed: resolve(__dirname, "../../reframed/index.ts"),
     },
   },
 });
