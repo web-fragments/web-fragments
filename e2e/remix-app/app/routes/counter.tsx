44d7d9118d5b95f6a9be884cbb611c8a206e618b
Dario Piotrowicz <dario@cloudflare.com>
2024-06-25 19:02:24 +0200

    make counter route conditionally render counter or fragment

--- e2e/remix-app/app/routes/counter.tsx
+++ e2e/remix-app/app/routes/counter.tsx
@@ -1,54 +1,37 @@
 import { useRef, useState, useEffect } from "react";
 
-export function loader() {
-  // TODO...
-  return false;
-}
-
 export default function Counter() {
+  const [shouldReframe, setShouldReframe] = useState(false);
   const [counter, setCounter] = useState(0);
 
   const ref = useRef<HTMLElement>(null);
 
-  if (!globalThis.document?.unreframedBody || (!globalThis.document && isThisAFetchRequestFromReframed) ) {
-    useEffect(() => {
-        (async () => {
-          const {reframed} = await import('reframed');
-          const reframedContainer = ref.current;
-          if (!reframedContainer) return;
-          await reframed("/counter", { container: reframedContainer });
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      if (!document.unreframedBody) {
+        setShouldReframe(true);
+      }
 
-    return (
-      <>
-        <h1>hello from parent remix</h1>
-        <article
-          style={{
-            border: "6px dashed red",
-            scale: "60%",
-            display: "grid",
-            placeContent: "center",
-          }}
-          ref={ref}
-        ></article>
-      </>
-    )
-  }
+      const { reframed } = await import("reframed");
+      const reframedContainer = ref.current;
+      if (!reframedContainer) return;
+      await reframed("/counter", { container: reframedContainer });
+    })();
+  }, []);
 
-  return (
+  return !shouldReframe ? (
     <div>
       <style>{`
-        .counter {
-            margin: 1rem;
-            display: flex;
-            gap: 1rem;
-            width: 15rem;
-            background: #e4e4e4;
-            justify-content: space-between;
-            padding: 0.5rem;
-        }
-      `}</style>
+    .counter {
+        margin: 1rem;
+        display: flex;
+        gap: 1rem;
+        width: 15rem;
+        background: #e4e4e4;
+        justify-content: space-between;
+        padding: 0.5rem;
+    }
+  `}</style>
       <div className="counter">
         <button
           onClick={() => {
@@ -67,5 +50,18 @@ export default function Counter() {
         </button>
       </div>
     </div>
+  ) : (
+    <>
+      <h1>hello from parent remix</h1>
+      <article
+        style={{
+          border: "6px dashed red",
+          scale: "60%",
+          display: "grid",
+          placeContent: "center",
+        }}
+        ref={ref}
+      ></article>
+    </>
   );
 }
