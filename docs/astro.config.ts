b7d84533db29c3156df826d4be23cd5af3cba3e3
Igor Minar <iminar@cloudflare.com>
2025-01-27 16:26:17 -0800

    style: applying the new formatting config to docs/
    This commit just applies the config updated in the previous commit.

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -13,88 +13,78 @@ import type { AstroIntegration } from "astro";
 import astrowind from "./vendor/integration";
 import remarkMermaid from "remark-mermaidjs";
 
-import {
-  readingTimeRemarkPlugin,
-  responsiveTablesRehypePlugin,
-  lazyImagesRehypePlugin,
-} from "./src/utils/frontmatter";
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from "./src/utils/frontmatter";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const hasExternalScripts = false;
-const whenExternalScripts = (
-  items: (() => AstroIntegration) | (() => AstroIntegration)[] = [],
-) =>
-  hasExternalScripts
-    ? Array.isArray(items)
-      ? items.map((item) => item())
-      : [items()]
-    : [];
+const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroIntegration)[] = []) =>
+	hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  output: "static",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    mdx(),
-    icon({
-      include: {
-        tabler: ["*"],
-        "flat-color-icons": [
-          "advertising",
-          "approval",
-          "bearish",
-          "business-contact",
-          "currency-exchange",
-          "database",
-          "document",
-          "gallery",
-          "org-unit",
-          "template",
-          "voice-presentation",
-        ],
-      },
-    }),
+	output: "static",
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		mdx(),
+		icon({
+			include: {
+				tabler: ["*"],
+				"flat-color-icons": [
+					"advertising",
+					"approval",
+					"bearish",
+					"business-contact",
+					"currency-exchange",
+					"database",
+					"document",
+					"gallery",
+					"org-unit",
+					"template",
+					"voice-presentation",
+				],
+			},
+		}),
 
-    ...whenExternalScripts(() =>
-      partytown({
-        config: { forward: ["dataLayer.push"] },
-      }),
-    ),
+		...whenExternalScripts(() =>
+			partytown({
+				config: { forward: ["dataLayer.push"] },
+			}),
+		),
 
-    compress({
-      CSS: true,
-      HTML: {
-        "html-minifier-terser": {
-          removeAttributeQuotes: false,
-        },
-      },
-      Image: false,
-      JavaScript: true,
-      SVG: false,
-      Logger: 1,
-    }),
+		compress({
+			CSS: true,
+			HTML: {
+				"html-minifier-terser": {
+					removeAttributeQuotes: false,
+				},
+			},
+			Image: false,
+			JavaScript: true,
+			SVG: false,
+			Logger: 1,
+		}),
 
-    astrowind({
-      config: "./src/config.yaml",
-    }),
-  ],
+		astrowind({
+			config: "./src/config.yaml",
+		}),
+	],
 
-  image: {
-    domains: ["cdn.pixabay.com"],
-  },
-  markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin, remarkMermaid],
-    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
-  },
+	image: {
+		domains: ["cdn.pixabay.com"],
+	},
+	markdown: {
+		remarkPlugins: [readingTimeRemarkPlugin, remarkMermaid],
+		rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
+	},
 
-  vite: {
-    resolve: {
-      alias: {
-        "~": path.resolve(__dirname, "./src"),
-      },
-    },
-  },
+	vite: {
+		resolve: {
+			alias: {
+				"~": path.resolve(__dirname, "./src"),
+			},
+		},
+	},
 });
--- docs/src/pages/documentation/elements.md
+++ docs/src/pages/documentation/elements.md
@@ -18,8 +18,8 @@ import { FragmentOutlet } from "./fragment-outlet";
 import { FragmentHost } from "./fragment-host";
 
 export function register() {
-  window.customElements.define("fragment-outlet", FragmentOutlet);
-  window.customElements.define("fragment-host", FragmentHost);
+	window.customElements.define("fragment-outlet", FragmentOutlet);
+	window.customElements.define("fragment-host", FragmentHost);
 }
 ```
 
