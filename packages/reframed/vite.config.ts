c51b257c61912ee85c36c4face33e53eb892d39b
Natalia Venditto <nataliafrontend@gmail.com>
2025-02-04 17:55:16 +0100

    refactor: fix issue with env not recognized as part of ImportMeta interface

--- packages/reframed/vite.config.ts
+++ packages/reframed/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		lib: {
-			entry: new URL(import.meta.resolve('./index.ts')).pathname,
+			entry: import.meta.resolve ? new URL(import.meta.resolve('./index.ts')).pathname : './index.ts',
 			fileName: 'reframed',
 			formats: ['es'],
 		},
