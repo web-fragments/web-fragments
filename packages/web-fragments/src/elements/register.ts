a4a74697b0d92034c21608b5e81547de27b7349b
Natalia Venditto <nataliafrontend@gmail.com>
2025-02-14 14:11:45 +0100

    feat: rename register function

--- packages/web-fragments/src/elements/register.ts
+++ packages/web-fragments/src/elements/register.ts
@@ -1,7 +1,14 @@
 import { FragmentOutlet } from './fragment-outlet';
 import { FragmentHost } from './fragment-host';
 
-export function register() {
+export function registerWebFragmentElements() {
 	window.customElements.define('fragment-outlet', FragmentOutlet);
 	window.customElements.define('fragment-host', FragmentHost);
+}
+
+// keep for retro compatibility
+// TODO: write test when suites are merged
+export function register() {
+	console.warn('[⚠️ Web Fragments Info]: register() is deprecated and will be removed in the future, please use registerFragmentElements() instead');
+	registerWebFragmentElements();
 }
