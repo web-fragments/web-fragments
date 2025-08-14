3dedf11dd1e34ce2f1cfc825b4f467f6763fc29a
Igor Minar <iminar@cloudflare.com>
2025-08-13 22:45:45 -0700

    fixup! wip: improve pierced preserveStyle

--- packages/web-fragments/test/playground/piercing/spec.ts
+++ packages/web-fragments/test/playground/piercing/spec.ts
@@ -7,15 +7,22 @@ beforeEach(failOnBrowserErrors);
 let fragment: Locator;
 let fragmentContext: Frame;
 
-beforeEach(async ({ page }) => {
-	await page.goto('/piercing/');
-	// wait for the fragment to load
-	await page.waitForSelector('web-fragment h2');
+if (process.env.PIERCING === 'true') {
+	// Only run these tests if piercing is enabled
+	// All of these tests assume that the host was pierced.
 
-	fragment = page.locator('web-fragment');
-	fragmentContext = await getFragmentContext(fragment);
-});
+	beforeEach(async ({ page }) => {
+		await page.goto('/piercing/');
+		// wait for the fragment to load
+		// we use web-fragment-host because piercing needs to be triggered manually in this test
+		await page.waitForSelector('web-fragment-host h2');
 
-test('should pierced the fragment', async ({ page }) => {
-	expect(await fragmentContext.evaluate(`document.querySelector('h2').textContent`)).toBe('hello world!');
-});
+		// we use web-fragment-host because piercing needs to be triggered manually in this test
+		fragment = page.locator('web-fragment-host');
+		fragmentContext = await getFragmentContext(fragment);
+	});
+
+	test('should pierced the fragment', async ({ page }) => {
+		expect(await fragmentContext.evaluate(`document.querySelector('h2').textContent`)).toBe('Piercing style tests!');
+	});
+}
