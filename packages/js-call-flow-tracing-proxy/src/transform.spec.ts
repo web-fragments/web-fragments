330a749c9d27a7f45c45a8ebab01b56d1e2ef903
Igor Minar <i@igor.dev>
2025-10-31 09:44:28 -0700

    feat(jscftp): instrument function entry

--- packages/js-call-flow-tracing-proxy/src/transform.spec.ts
+++ packages/js-call-flow-tracing-proxy/src/transform.spec.ts
@@ -5,7 +5,7 @@ describe('transform', () => {
 	it('should transform a simple function', () => {
 		const code = 'function foo() { return 1; }';
 		const result = transform(code, 'test.js');
-		expect(result.code).toBe('function foo() { return 1; }\nconsole.log("Hello World!! (from test.js)");');
+		expect(result.code).toBe('function foo() {\n  console.debug("test.js: → foo@1:0");\n  return 1;\n}');
 		expect(result.sourceMap).toBeDefined();
 	});
 });
--- packages/js-call-flow-tracing-proxy/src/transform.ts
+++ packages/js-call-flow-tracing-proxy/src/transform.ts
@@ -6,7 +6,8 @@ export function transform(code: string, sourceFileName: string): { code: string;
 		sourceFileName: sourceFileName,
 	});
 
-	appendHelloWorld(ast.program.body, sourceFileName);
+	//appendHelloWorld(ast.program.body, sourceFileName);
+	instrument(ast.program.body, sourceFileName);
 
 	const transformed = print(ast, {
 		sourceMapName: sourceFileName,
@@ -17,6 +18,26 @@ export function transform(code: string, sourceFileName: string): { code: string;
 		code: transformed.code,
 		sourceMap: transformed.map,
 	};
+}
+
+function instrument(astBody: types.ASTNode[], sourceFileName: string) {
+	types.visit(astBody, {
+		visitFunctionDeclaration(path) {
+			const functionEntryLog = types.builders.callStatement.from({
+				callee: types.builders.identifier.from({ name: 'console.debug' }),
+				arguments: [
+					types.builders.literal.from({
+						value: `${sourceFileName}: → ${path.node.id?.name}@${path.node.loc?.start.line}:${path.node.loc?.start.column}`,
+					}),
+				],
+			});
+
+			const functionDeclaration = path.node;
+
+			functionDeclaration.body.body.unshift(functionEntryLog);
+			this.traverse(path);
+		},
+	});
 }
 
 function appendHelloWorld(astBody: types.ASTNode[], sourceFileName: string) {
